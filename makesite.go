e282e9761f735e3de07870d41aced883af2158b0
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-02-02 18:51:05 -0800

    moved create to its own function

--- makesite.go
+++ makesite.go
@@ -23,34 +23,64 @@ func check(e error) {
 	}
 }
 
-func main() {
-
-	// Some sandbox set up
-	// file := "first-post.txt"
-	filePtr := flag.String("file", "sample.txt", "a text file")
-	flag.Parse()
-	file := *filePtr
-
+func createHTMLFromTemplate(file string, tmpl string) {
+	// Some filepath wrangling
 	ext := filepath.Ext(file)
-	filename := strings.TrimSuffix(file, ext)
+	base := filepath.Base(file)
+	filename := strings.TrimSuffix(base, ext)
 
 	// Reading
 	fileContents, err := ioutil.ReadFile(file)
 	check(err)
 
+	// Assumes first line contains title and separated by body contents by 1+ newline
 	fileSplit := strings.SplitN(string(fileContents), "\n", 2)
-	e := Entry{
+
+	ent := Entry{
 		Title: fileSplit[0],
 		Body:  strings.TrimSpace(fileSplit[1]),
 	}
+
 	// Create new file
-	w, err := os.Create(filename + ".html")
+	postDir := "posts/"
+	w, err := os.Create(filepath.Join(postDir, filename) + ".html")
 	check(err)
 
 	// Template stuff
-	t := template.Must(template.ParseFiles("templates/template.tmpl", "templates/header.tmpl"))
-	t.ExecuteTemplate(w, "template.tmpl", e)
+	tmplDir := tmpl
+	t := template.Must(template.ParseFiles(filepath.Join(tmplDir, "template.tmpl"), filepath.Join(tmplDir, "header.tmpl")))
+	t.ExecuteTemplate(w, "template.tmpl", ent)
 	check(err)
 
 	fmt.Printf(filename + ".html saved successfully")
+}
+
+func main() {
+
+	// Flags
+	filePtr := flag.String("file", "sample.txt", "a text file to convert")
+	// dirPtr := flag.String("dir", ".", "a directory of text files to convert")
+	tDirPtr := flag.String("templates", "./templates", "a directory of templates")
+
+	flag.Parse()
+
+	// Point parameters to flags
+	files := *filePtr
+	// txtDir := *dirPtr
+	tmplDir := *tDirPtr
+	info, err := os.Stat(files)
+	check(err)
+	if info.IsDir() {
+
+		files, err := ioutil.ReadDir(files)
+		check(err)
+		for _, file := range files {
+			fmt.Printf("%s \n", file.Name())
+			// createHTMLFromTemplate(filepath.Abs(file), tmplDir)
+		}
+	} else {
+		fmt.Printf("file: ")
+		createHTMLFromTemplate(files, tmplDir)
+	}
+
 }
